64fe9acace2660fc0700f9475d17d6adc8803ede
PhilemonVanDussen <vandussenph709@northwested.org>
2024-11-25 13:57:44 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+# PJ VanDussen
+# 11/25/24
+# While loops
+
+# Guess game
+# import random
+
+# name = input('Hello what is your name? ')
+# number = random.randint(1, 100)
+# print(f'Hi {name} im thinking od a number between 1 and 100.')
+# guesses_taken = 0
+
+# while guesses_taken < 5:
+#     guess = input('Enter a guess: ')
+#     guess = int(guess)
+#     guesses_taken = guesses_taken + 1
+#     if guess < number:
+#         print('That was too low.')
+#     elif guess > number:
+#         print('That was too high')
+#     else:
+#         break
+   
+# if guess == number:
+#     print(f'You won {name}!!!!!!!!!!!!!!! Good Job')
+# else:
+#     print(f'You lose, the number was {number}, try again')
+
+# Average Temperature
+temps = []
+user_int = 0
+while user_int > -9999 :
+    user_int = int(input('Enter a temperature in Fahrenheit (-9999 to quit) '))
+    temps.append(user_int)
+
+list_length = len(temps)
+if list_length > 0:
+    print(f'Temperatures entered: {temps}')
+    average_temp = sum(temps) / len(temps)
+    print(f'Average temperature: {average_temp}')
+else:
+    print('No temperatures entered')
+
+
+    
+
+
+
+
+
